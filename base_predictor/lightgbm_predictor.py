7bca82c99b43320e219ab8c366aba43c1fd18313
Jay Lee <jayaos1002@gmail.com>
2026-04-04 14:38:32 -0400

    bugfix

--- base_predictor/lightgbm_predictor.py
+++ base_predictor/lightgbm_predictor.py
@@ -61,8 +61,8 @@ class LightGBMPredictor:
             print("making predictions on heldout set...")
             all_x = concatenate([item["train_x"], item["heldout_x"]], axis="time")
             past_x = all_x[len(item["train_x"])-past_window:]
-            print(past_x.components)
-            
+            print(past_x.components, flush=True)
+
             predictions = model.predict(len(item["heldout_y"]), 
                                         past_covariates=past_x,
                                         show_warnings=False) 
