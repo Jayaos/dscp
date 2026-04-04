b5760e5a6e2db8c69fd1507998f68cdbb0a327d4
Jay Lee <jayaos1002@gmail.com>
2026-04-04 14:32:16 -0400

    bugfix

--- base_predictor/lightgbm_predictor.py
+++ base_predictor/lightgbm_predictor.py
@@ -35,7 +35,7 @@ class LightGBMPredictor:
             train_x, heldout_x = x_ts.split_before(self.train_ratio)
             train_y, heldout_y = y_ts.split_before(self.train_ratio)
 
-            print(train_x)
+            print(train_x.components)
 
             self.data_darts_format[key] = {"train_x" : train_x,
                                       "heldout_x" : heldout_x,
